f1b43b0b8f0714ee0877b727b7e9b1d85fe576d8
virg1n <113487825+virg1n@users.noreply.github.com>
2023-03-20 22:05:37 +0600

    Convolutional mnist determinant

---
+++ Convolutional.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.datasets import mnist
+import numpy as np
+import keras
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train / 255
+x_test = x_test / 255
+
+x_train = np.expand_dims(x_train, axis=3)
+x_test = np.expand_dims(x_test, axis=3)
+
+y_train_cat = keras.utils.to_categorical(y_train, 10)
+y_test_cat = keras.utils.to_categorical(y_test, 10)
+
+model = keras.Sequential(
+    [
+        Conv2D(32, (3,3), padding="same", activation="relu", input_shape=(28, 28, 1)),
+        MaxPooling2D((2,2), strides=2),
+        Conv2D(64, (3,3), padding="same", activation="relu"),
+        MaxPooling2D((2,2), strides=2),
+        Flatten(),
+        Dense(128, activation="relu"),
+        Dense(10, activation="softmax")
+    ]
+)
+
+model.compile(optimizer="adam",
+              loss="categorical_crossentropy",
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train_cat, batch_size=32, epochs=5, validation_split=0.2)
+
+model.evaluate(x_test, y_test_cat)
